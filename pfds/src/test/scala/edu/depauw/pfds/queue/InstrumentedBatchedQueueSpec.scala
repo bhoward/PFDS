21974fdd9d98bfdf1c0ca7a9cd9a681c083b6a35
Brian Howard <bhoward@depauw.edu>
2018-09-03 12:59:46 -0400

    Added queue persistence/non-persistence tests

--- pfds/src/test/scala/edu/depauw/pfds/queue/InstrumentedBatchedQueueSpec.scala
+++ pfds/src/test/scala/edu/depauw/pfds/queue/InstrumentedBatchedQueueSpec.scala
@@ -2,6 +2,7 @@ package edu.depauw.pfds.queue
 
 import org.scalatest._
 import org.scalatest.prop.PropertyChecks
+import org.scalacheck.Gen
 
 import edu.depauw.pfds.instrumentation.Recorder
 
@@ -9,16 +10,38 @@ final class InstrumentedBatchedQueueSpec extends PropSpec with PropertyChecks wi
   property("Queue should deliver all values in amortized constant time") {
     implicit val recorder = new Recorder
     var q: Queue[Int] = InstrumentedBatchedQueue()
-    
+
     for (i <- 1 to 1000) {
       q = q.enqueue(i)
     }
-    
+
     for (i <- 1 to 1000) {
-      q.head should be (Some(i))
+      q.head should be(Some(i))
       q = q.dequeue.get
     }
-    
+
     recorder.maximumAverageTicksPerOp should (be <= 2)
+  }
+
+  val sizes = for (n <- Gen.choose(10, 10000)) yield n
+
+  property("Queue is not persistent") {
+    forAll(sizes) { (n: Int) =>
+      whenever(10 <= n && n <= 10000) {
+        implicit val recorder = new Recorder
+        var q: Queue[Int] = InstrumentedBatchedQueue()
+
+        for (i <- 1 to n) {
+          q = q.enqueue(i)
+        }
+
+        for (_ <- 1 to n) {
+          val q2 = q.dequeue.get
+          q2.head should be(Some(2))
+        }
+
+        recorder.maximumAverageTicksPerOp should (be > n / 4)
+      }
+    }
   }
 }
